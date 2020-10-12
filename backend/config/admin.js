8365155dbb54f44c7852056bd827c609d65d74f0
Leandro Santos <61752589+Leolucas12@users.noreply.github.com>
2020-10-12 14:51:36 -0300

    Update admin.js

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,4 +1,5 @@
 module.exports = middleware => {
+    // checks if user is an administrator by readng the request
     return (req, res, next) => {
         if (req.user.admin) {
             middleware(req, res, next)
